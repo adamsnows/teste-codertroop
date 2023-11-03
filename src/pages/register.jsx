49fd314233352a037bcf761be950cff09653e811
Adam N <adaamangelow@gmail.com>
2023-11-03 00:29:19 -0300

    feat: title on register

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import Register from "../components/Register";
+import Head from "next/head";
 
 const RegisterPage = () => {
   return (
     <div className="max-w-full min-h-screen flex justify-center items-center backgroundImage bg-bluepattern bg-no-repeat bg-cover">
+      <Head>
+        <title>CoderTroop - Registro</title>
+      </Head>
       <Register />
     </div>
   );
